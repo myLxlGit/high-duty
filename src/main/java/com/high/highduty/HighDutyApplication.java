1a70427310a667fa45191772ffa344657bd3139a
liuxiaolong <398511767@qq.com>
2023-06-26 18:39:11 +0800

    Initial commit

---
+++ src/main/java/com/high/highduty/HighDutyApplication.java
@@ -0,0 +1,15 @@
+package com.high.highduty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighDutyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighDutyApplication.class, args);
+
+        System.out.println("-------------------启动成功------------------------");
+    }
+
+}
---
+++ src/test/java/com/high/highduty/HighDutyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.high.highduty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighDutyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
