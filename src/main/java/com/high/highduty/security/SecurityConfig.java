35517d165018ec505199cd35779d2e7eed74adce
liuxiaolong <398511767@qq.com>
2023-06-27 16:23:21 +0800

    删除无用引包

--- src/main/java/com/high/highduty/security/SecurityConfig.java
+++ src/main/java/com/high/highduty/security/SecurityConfig.java
@@ -2,9 +2,7 @@ package com.high.highduty.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
